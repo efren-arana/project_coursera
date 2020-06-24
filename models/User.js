e86ea6d78f3b7e39b1f09a23de08a0244a343996
Efren Arana <efren-f@hotmail.com>
2020-06-23 19:20:28 -0500

    commillas simple al JWT_KEY

--- models/User.js
+++ models/User.js
@@ -74,7 +74,7 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.generateAuthToken = async function() {
     // Generate an auth token for the user
     const user = this
-    const jwt_key = process.env.JWT_KEY || WinterIsComingGOT2019
+    const jwt_key = process.env.JWT_KEY || 'WinterIsComingGOT2019'
     const payload = {_id: user._id,iat:moment().unix(),exp:moment().add(60,'minutes').unix()}
     console.log(payload);
     const token = jwt.sign(payload, jwt_key) //metodo para generar el token
