741917f558b3fed13c87d20a444cf168556ac0d3
efren-arana <59148881+efren-arana@users.noreply.github.com>
2020-11-23 22:02:23 -0500

    Delete environment.js

--- environment.js
+++
@@ -1,6 +0,0 @@
-//cambiar url de la base de datos por una variable en el servidor heroku
-export const environment = {
-    port: process.env.PORT || 5000,
-    url : process.env.MONGODB_URL || 'mongodb+srv://dbProjectEfren:12345@cluster0-cqwe4.mongodb.net/user-registration-db',
-    JWT_KEY: process.env.JWT_KEY || 'WinterIsComingGOT2019'
-}
